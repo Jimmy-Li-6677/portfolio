76eac3fcbecf9a36a67d0dee4a376def214c4367
JIMMY <jimmy@JIMMYs-MBP.fios-router.home>
2025-05-31 11:26:46 -0400

    123

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,11 +3,13 @@ import viteLogo from '/vite.svg'
   import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import AboutMe from './pages/AboutMe'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/aboutme" element={<AboutMe />} />
     </Routes>
   )
 }
--- portfolio/src/components/ClickButton.jsx
+++ portfolio/src/components/ClickButton.jsx
@@ -4,18 +4,12 @@ const ClickButton = ({ title, onClick, className }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    if (title === "GYMMATE") {
-      navigate('/');
-    } else if (title === 'Sign up') {
-      navigate('/signup');
-    } else if (title === 'Diet Plan') {
-      navigate('/dietplan');
-    } else if (title === 'Workout Plan') {
-      navigate('/workoutplan');
-    } else if (title === 'Nutrition') {
-      navigate('/nutrition');
-    } else if (title === 'snowboard_video') {
+    if (title === 'snowboard_video') {
       window.open('https://youtube.com/shorts/Pc2pq-Xzyrs?feature=share', '_blank');
+    } else if (title ==='About Me') {
+      navigate('/aboutme');
+    } else if (title ==='Portfolio') {
+      navigate('/');
     }else {
       onClick?.();
     }
